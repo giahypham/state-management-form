c104b9852ca5c332dedc9c4205c79595da0cbbc7
giahypham <andygpham@gmail.com>
2023-09-21 23:59:01 -0400

    Finishes Wheel.js

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,9 +1,18 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE, SET_QUIZ_INTO_STATE, SET_SELECTED_ANSWER, SET_INFO_MESSAGE, INPUT_CHANGE, RESET_FORM  } from './action-types'
+
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
+  switch(action.type) {
+    case MOVE_CLOCKWISE:
+      return (state + 1) % 6 ; //Modulo 6 to keep the value in the range 0-5 otherwise it would go out of the grid
+    case MOVE_COUNTERCLOCKWISE:
+      return (state - 1 + 5) % 6 ; //+5 to keep the value from going negative
+    default: 
+      return state;
+  }
 }
 
 const initialQuizState = null
